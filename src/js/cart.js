9aa1e10d32da9c0fe979e212ceffe88ad6f8b1cc
NadiaStyslo <sir.ihor@gmail.com>
2023-12-25 08:00:06 -0500

    worked

--- src/js/cart.js
+++ src/js/cart.js
@@ -21,3 +21,90 @@ totalPrice.textContent = `$${total}`
 // const moreBtn = document.querySelectorAll('.more-btn')
 // const lessBtn = document.querySelectorAll('.less-btn')
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const removeBtn = document.querySelector('.remove-btn')
+
+
+for (var i = 0; i < removeBtn.length; i = +1) {
+    var delBtn = removeBtn[i];
+    
+    delBtn.addEventListener('click', function (evt) {
+        const cartItemRemove = evt.target.dataset.productdId;
+        console.log(evt.target.dataset.productdId)
+     const updatedProducts = products.filter(item => item.product !== cartItemRemove);
+   
+      localStorage.setItem(KEY, JSON.stringify(updatedProducts));  
+    
+        updateCart(container.inserHTML(createMarkupCart(products)))
+          })
+}
+        // carItemsRemove = localStorage.getItem(KEY);
+        // carItemsRemove = JSON.parse(carItemsRemove);
+       
+        // for (var i in carItemsRemovee) {
+        //     if (carItemsRemove != null) {
+        //         localStorage.removeItem('title');
+        //         localStorage.removeItem('total');
+        //         localStorage.removeItem(KEY, carItemsRemove[i])
+        //     }
+        // }
+  
+//         //console.log(evt.target.dataset)
+//         if (products && products[carItemsRevove]) {
+//             delete products[carItemsRevove];
+//             localStorage.setItem(KEY, JSON.stringify(products));
+//             updataCart();
+            
+//         }
+//        updataCart
+
+        
+        
+        // localStorage.getItem('products');
+        // carItems = JSON.parse(carItems);
+        // Object.values(carItems).map(item => {
+        //     item.id = 0;
+           
+        //     localStorage.removeItem('products')
+        
+        // })
+        // location.reload()
+// })
+// }
+
